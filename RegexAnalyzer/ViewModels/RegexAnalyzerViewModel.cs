f114d4562c45e0cfe40f88c85d0e4ab3c5ffa0b9
Arthur Mauricio <ar057223@intelbras.com.br>
2023-05-07 00:11:30 -0300

    Added State Machine and Generich Alg.

--- RegexAnalyzer/ViewModels/RegexAnalyzerViewModel.cs
+++ RegexAnalyzer/ViewModels/RegexAnalyzerViewModel.cs
@@ -20,9 +20,20 @@ namespace RegexAnalyzer.ViewModels
         private string? _regexSyntax;
         private string? _regexOutput;
         public ObservableCollection<RegSyntax> _syntaxList = new ObservableCollection<RegSyntax>();
-        private string[] arithmath = new string[] { "+", "-", "/", "*", "%" };
-        int[] EF = new int[] { 0, 0, 0, 0, 1, 0, 0, 1, 0,0 };
-        
+        private char[] arithmath = new char[] { '+', '-', '/', '*'};
+        private int[] _ef = new int[] { 0, 0, 0, 0, 1, 0, 0, 1, 0 };
+        private int[,] _stateMachine = new int[,] { 
+           // A   B   C   D   E
+            { 1, -1, -1, -1, -1 }, //E0
+            { -1, 2, -1, -1, -1 }, //E1
+            { 3, -1, -1, -1, -1 }, //E2
+            { -1, -1, 4, 5, -1 }, //E3
+            { 6, -1, -1, -1, -1 }, //E4*
+            { -1, -1, -1, -1, 7 }, //E5
+            { -1, -1, -1, -1, 7 }, //E6*
+            { -1, -1, -1, -1, -1 }, //E7
+        }; // Tabela de transição para a linguagem L
+
         public FlatTreeDataGridSource<RegSyntax> Source { get; } //This allow View to retrive info from Source
 
         public RegexAnalyzerViewModel()
@@ -50,24 +61,267 @@ namespace RegexAnalyzer.ViewModels
             }
         }
 
+        private void CheckSyntax(bool isControlChar = false)
+        {
+
+        }
         public void SyntaxAnalyzer()
         {
 
             try
             {
-                RegSyntax novousuario = new RegSyntax("ababcdede", "Sentença Valida");
-                RegSyntax novousuario1 = new RegSyntax("+", "Operador Aritmético");
-                RegSyntax novousuario2 = new RegSyntax("ccc", "Sentença Valida");
-                RegSyntax novousuario3 = new RegSyntax("ababababde", "Sentença Valida");
+                int currentIndex = 0;
+                int state = 0;
+                char symbol = _regexSyntax[currentIndex];
+                string currentSyntax = "";
+                bool invalidSyntax = false;
+                bool invalidSymbol = false;
+                while (currentIndex < _regexSyntax.Length)
+                {
+                    int column;
+                    if (symbol == 'a')
+                    {
+                        column = 0;
+                    }
+                    else if (symbol == 'b')
+                    {
+                        column = 1;
+                    }
+                    else if (symbol == 'c')
+                    {
+                        column = 2;
+                    }
+                    else if (symbol == 'd')
+                    {
+                        column = 3;
+                    }
+                    else if (symbol == 'e')
+                    {
+                        column = 4;
+                    }
+                    else
+                    {
+                        column = -1;
+                    }
 
-                _syntaxList.Add(novousuario);
-                _syntaxList.Add(novousuario1);
-                _syntaxList.Add(novousuario2);
-                _syntaxList.Add(novousuario3);//This will add new RegSyntax object to _syntaxList.
+                    if (column == -1)
+                    {
+                        if (char.IsControl(symbol) || arithmath.Contains(symbol) || char.IsWhiteSpace(symbol))
+                        {
+                            if (invalidSyntax || invalidSymbol)
+                            {
+                                if (invalidSyntax)
+                                {
+                                    RegSyntax syntax = new RegSyntax(currentSyntax, "ERRO – Sentença Inválida");
+                                    _syntaxList.Add(syntax);
+                                }
+                                else
+                                {
+                                    RegSyntax syntax = new RegSyntax(currentSyntax, "ERRO – Símbolo(s) Inválido(s):");
+                                    _syntaxList.Add(syntax);
+                                }
+                                currentSyntax = "";
+                                invalidSymbol = false;
+                                invalidSyntax = false;
+                            }
+                            else
+                            {
+                                if (!String.IsNullOrEmpty(currentSyntax))
+                                {
+                                    if (_ef[state] == 1)
+                                    {
+                                        RegSyntax syntax = new RegSyntax(currentSyntax, "Sentença Válida.");
+                                        _syntaxList.Add(syntax);
+                                    }
+                                    else
+                                    {
+                                        RegSyntax syntax = new RegSyntax(currentSyntax, "ERRO – Sentença Inválida");
+                                        _syntaxList.Add(syntax);
+                                    }
+                                }
+                                currentSyntax = "";
+                                invalidSymbol = false;
+                                invalidSyntax = false;
+                            }
+                            if (arithmath.Contains(symbol))
+                            {
+                                RegSyntax syntax = new RegSyntax(symbol.ToString(), "Operador Aritmético.");
+                                _syntaxList.Add(syntax);
+                                currentSyntax = "";
+                                invalidSymbol = false;
+                                invalidSyntax = false;
+                            }
+
+                        }
+                        else
+                        {
+                            currentSyntax += symbol.ToString();
+                            if (currentSyntax.Length <= 1 && !invalidSyntax)
+                            {
+                                invalidSymbol = true;
+                            }
+                            else
+                            {
+
+                                invalidSyntax = true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        currentSyntax += symbol.ToString();
+                        if (!invalidSyntax)
+                        {
+                            state = _stateMachine[state, column];
+                            if(state == -1)
+                            {
+                                invalidSyntax= true;
+                            }
+                        }
+                    }
+
+
+
+                    if (currentIndex + 1 == _regexSyntax.Length)
+                    {
+
+                        if(state == -1)
+                        {
+                            if (invalidSymbol)
+                            {
+                                RegSyntax syntax = new RegSyntax(currentSyntax, "ERRO – Símbolo(s) Inválido(s):");
+                                _syntaxList.Add(syntax);
+                            }
+                            else
+                            {
+                                RegSyntax syntax = new RegSyntax(currentSyntax, "ERRO – Sentença Inválida.");
+                                _syntaxList.Add(syntax);
+                            }
+                        }
+                        else if (_ef[state] == 1 & !invalidSyntax & !invalidSymbol)
+                        {
+                            RegSyntax syntax = new RegSyntax(currentSyntax, "Sentença Válida.");
+                            _syntaxList.Add(syntax);
+                        }
+                        else
+                        {
+                            if (invalidSymbol)
+                            {
+                                RegSyntax syntax = new RegSyntax(currentSyntax, "ERRO – Símbolo(s) Inválido(s):");
+                                _syntaxList.Add(syntax);
+                            }
+                            else
+                            {
+                                RegSyntax syntax = new RegSyntax(currentSyntax, "ERRO – Sentença Inválida.");
+                                _syntaxList.Add(syntax);
+                            }
+                        }
+                        currentIndex++;
+                    }
+                    else
+                    {
+                        symbol = _regexSyntax[currentIndex + 1];
+                        currentIndex++;
+                    }
+
+
+
+
+
+
+                    //if (column == -1)
+                    //{
+
+                    //    if (char.IsControl(symbol) || arithmath.Contains(symbol) || char.IsWhiteSpace(symbol))
+                    //    {
+                    //        if (invalidSyntax || invalidSymbol)
+                    //        {
+                    //            if (invalidSyntax)
+                    //            {
+                    //                RegSyntax syntax = new RegSyntax(currentSyntax, "ERRO – Sentença Inválida");
+                    //                _syntaxList.Add(syntax);
+                    //            }
+                    //            else
+                    //            {
+                    //                RegSyntax syntax = new RegSyntax(currentSyntax, "ERRO – Símbolo(s) Inválido(s):");
+                    //                _syntaxList.Add(syntax);
+                    //            }
+                    //            currentSyntax = "";
+                    //            invalidSymbol = false;
+                    //            invalidSyntax = false;
+                    //        }
+                    //        else
+                    //        {
+                    //            if (!String.IsNullOrEmpty(currentSyntax))
+                    //            {
+                    //                if(_ef[state] == 1)
+                    //                {
+                    //                    RegSyntax syntax = new RegSyntax(currentSyntax, "Sentença Válida.");
+                    //                    _syntaxList.Add(syntax);
+                    //                }
+                    //                else
+                    //                {
+                    //                    RegSyntax syntax = new RegSyntax(currentSyntax, "ERRO – Sentença Inválida");
+                    //                    _syntaxList.Add(syntax);
+                    //                }
+                    //            }
+                    //            currentSyntax = "";
+                    //            invalidSymbol = false;
+                    //            invalidSyntax = false;
+                    //        }
+                    //        if (arithmath.Contains(symbol))
+                    //        {
+                    //            RegSyntax syntax = new RegSyntax(symbol.ToString(), "Operador Aritmético.");
+                    //            _syntaxList.Add(syntax);
+                    //            currentSyntax = "";
+                    //            invalidSymbol = false;
+                    //            invalidSyntax= false;
+                    //        }
+
+                    //    }
+                    //    else
+                    //    {
+                    //        currentSyntax += symbol.ToString();
+                    //        if (currentSyntax.Length <= 1 && !invalidSyntax)
+                    //        {
+                    //            invalidSymbol = true;
+                    //        }
+                    //        else
+                    //        {
+
+                    //            invalidSyntax = true;
+                    //        }
+                    //    }
+
+                    //}
+                    //else
+                    //{
+                    //    currentSyntax += symbol.ToString();
+                    //}
+
+
+
+
+
+
+
+                    //if (column != -1)
+                    //{
+                    //    currentSyntax += symbol.ToString();
+                    //}
+                    //state = _stateMachine[state, column];
+                    //symbol = _regexSyntax[currentIndex + 1];
+                    //currentIndex++;
+
+                }
+
+                // FIM DO WHILE
+
+
             }
             catch (Exception e)
             {
-                Console.Write("Implementar");
+                
 
             }
 
